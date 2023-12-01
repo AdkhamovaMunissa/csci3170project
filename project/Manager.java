8daa40adf15ec736f75be05673b44a660d164e44
daniil2107 <55647901+daniil2107@users.noreply.github.com>
2023-12-01 21:54:01 +0800

    Update Manager.java

--- project/Manager.java
+++ project/Manager.java
@@ -25,14 +25,14 @@ public class Manager {
     
     public static void listSalesperson(Connection con, Statement selectStmt, ResultSet rs) {
         try {
-            System.out.println("| sID | sName | sAddress | sPhoneNumber | sExperience|");
+            System.out.println("| ID | Name | Mobile Phone | Years of Experience |");
             while (rs.next()) {
                 int sID = rs.getInt("sID");
                 String sName = rs.getString("sName");
-                String sAddress = rs.getString("sAddress");
+
                 int sPhoneNumber = rs.getInt("sPhoneNumber");
                 int sExperience = rs.getInt("sExperience");
-                System.out.println("| " + sID + " | " + sName + " | " + sAddress + " | " + sPhoneNumber + " | " + sExperience + " |");
+                System.out.println("| " + sID + " | " + sName + " | " + sPhoneNumber + " | " + sExperience + " |");
             }
 
             rs.close();
@@ -47,7 +47,7 @@ public class Manager {
             String l=String.valueOf(lower);
             String u=String.valueOf(upper);
             Statement selectStmt = con.createStatement();
-            ResultSet rs = selectStmt.executeQuery("SELECT s.sID, s.sName, s.sExperience, COUNT(t.tID) as numT FROM salesperson s JOIN transaction t ON s.sID = t.sID WHERE sExperience >= " + l + " AND sExperience <= " + u + " GROUP BY s.sID");
+            ResultSet rs = selectStmt.executeQuery("SELECT s.sID, s.sName, s.sExperience, COUNT(t.tID) as numT FROM salesperson s LEFT JOIN transaction t ON s.sID = t.sID WHERE sExperience >= " + l + " AND sExperience <= " + u + " GROUP BY s.sID");
             System.out.println("| ID | sName | Years of Experience | Number of Transactions |");
             while (rs.next()) {
                 int sID = rs.getInt("sID");
@@ -56,6 +56,7 @@ public class Manager {
                 int tID = rs.getInt("numT");
                 System.out.println("| " + sID + " | " + sName + " | " + sExperience + " | " + tID + " |");
             }
+            System.out.println("End of Query");
             rs.close();
             selectStmt.close();
         } catch (SQLException e) {
@@ -67,8 +68,8 @@ public class Manager {
         try {
             Statement selectStmt = con.createStatement();
             ResultSet rs = selectStmt.executeQuery("SELECT m.mID, m.mName, SUM(p.pPrice) as totalSales " +
-                                                   "FROM manufacturer m "+
-                                                   " JOIN part p ON m.mID = p.mID "+
+                                                   "FROM part p "+
+                                                   " JOIN manufacturer m ON p.mID = m.mID "+
                                                    "JOIN transaction t ON t.pID = p.pID " +
                                                    "GROUP BY m.mID" +
                                                    " ORDER BY totalSales DESC");
